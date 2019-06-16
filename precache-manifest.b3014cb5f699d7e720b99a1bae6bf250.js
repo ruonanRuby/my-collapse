66bfb95131f20f0e54a151b331fa4d5fc41c0d2f
ruonanRuby <rniu3@wisc.edu>
2019-06-15 19:50:50 -0500

    Updates

---
+++ precache-manifest.b3014cb5f699d7e720b99a1bae6bf250.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8632213f6bf29853c55feabff2a0e130",
+    "url": "/ruonanRuby/my-collapse.git/index.html"
+  },
+  {
+    "revision": "1cdaee45f7f1e9cd86b5",
+    "url": "/ruonanRuby/my-collapse.git/static/css/main.a7db123f.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/ruonanRuby/my-collapse.git/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "1cdaee45f7f1e9cd86b5",
+    "url": "/ruonanRuby/my-collapse.git/static/js/main.070860df.chunk.js"
+  },
+  {
+    "revision": "f2ba4412861d91861890",
+    "url": "/ruonanRuby/my-collapse.git/static/js/runtime~main.66a2a16d.js"
+  }
+]);
